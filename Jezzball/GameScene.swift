6a49916363be97490ca2aac4924f246fb2908751
Brad <brad@wewritecode.com>
2014-08-05 21:14:13 -0400

    adds some ball generation stuff

--- Jezzball/GameScene.swift
+++ Jezzball/GameScene.swift
@@ -1,36 +1,35 @@
 import SpriteKit
 
+let canvasWidth: UInt32 = 640
+let canvasHeight: UInt32 = 1136
+
+func newBall(x: CGFloat, y: CGFloat) -> SKSpriteNode {
+    let ballSprite = SKSpriteNode(imageNamed: "ball.png")
+
+    ballSprite.yScale = 0.25
+    ballSprite.xScale = 0.25
+    ballSprite.position = CGPointMake(x, y)
+    ballSprite.physicsBody = SKPhysicsBody(circleOfRadius: ballSprite.frame.size.width/2)
+    ballSprite.physicsBody.friction = 0.3
+    ballSprite.physicsBody.restitution = 0.8
+    ballSprite.physicsBody.mass = 0.0
+    ballSprite.physicsBody.allowsRotation = true
+
+    return ballSprite
+}
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        self.physicsWorld.gravity = CGVectorMake(-3, 0)
+        let physicsBody = SKPhysicsBody (edgeLoopFromRect: self.frame)
+        self.physicsBody = physicsBody
+
+        for i in 1..<20 {
+            let ball = newBall(CGFloat(arc4random()%(canvasWidth)), CGFloat(arc4random()%(canvasHeight)))
+            self.addChild(ball)
         }
     }
-   
+
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
